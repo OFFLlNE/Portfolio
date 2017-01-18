a8bf28f1ad5bc98586f79df6ce3e030c9aecf5db
OFFLlNE <valjalauri@gmail.com>
2017-01-18 18:31:37 +0200

    Get viewportHeight when document ready

--- Portfolio/scripts.js
+++ Portfolio/scripts.js
@@ -1,3 +1,8 @@
+$( document ).ready(function() {
+    var viewportHeight = $(window).height();
+$(".testPage").css("margin-top", viewportHeight)
+});
+
 $(window).resize(function(e) {
 	var viewportHeight = $(window).height();
 	$(".testPage").css("margin-top", viewportHeight)
