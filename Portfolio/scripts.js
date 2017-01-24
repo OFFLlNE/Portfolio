141fff897b2c964a6b898d71bb41e2a205fc1c95
OFFLlNE <valja.lauri@gmail.com>
2017-01-24 15:00:56 +0200

    Initial Look of the whole page.

--- Portfolio/scripts.js
+++ Portfolio/scripts.js
@@ -101,3 +101,21 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+
+const panels = document.querySelectorAll('.panel');
+
+    function toggleOpen() {
+      console.log('Hello');
+      this.classList.toggle('open');
+    }
+
+    function toggleActive(e) {
+      console.log(e.propertyName);
+      if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+      }
+    }
+
+    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
